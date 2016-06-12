87d9d90a83e23ce0eefef6cbb99910e152224383
hallahossam <hallahossam@gmail.com>
2016-06-12 13:06:36 +0300

    Added Work

--- app/src/main/java/com/example/halla/elmataamapp/Register/RegisterOneFragment.java
+++ app/src/main/java/com/example/halla/elmataamapp/Register/RegisterOneFragment.java
@@ -1,6 +1,8 @@
 package com.example.halla.elmataamapp.register;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -8,17 +10,32 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.example.halla.elmataamapp.ApplicationController;
 import com.example.halla.elmataamapp.R;
+
+import java.util.HashMap;
+import java.util.Map;
+
 
 /**
  * Created by Halla on 09/06/2016.
  */
 public class RegisterOneFragment extends Fragment implements View.OnClickListener {
+
+
     EditText email, password, confirmPassword, name, phone;
     Button save;
     CheckBox mCheckBox;
     String errorMessage = "Required";
+    SharedPreferences sharedPreferences;
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -42,11 +59,11 @@ public class RegisterOneFragment extends Fragment implements View.OnClickListene
         int id = v.getId();
         switch (id){
             case R.id.btn_save:
-                String Email = email.getText().toString();
-                String Pass = password.getText().toString();
+                final String Email = email.getText().toString();
+                final String Pass = password.getText().toString();
                 String Confirm = confirmPassword.getText().toString();
-                String Name = name.getText().toString();
-                String Phone = phone.getText().toString();
+                final String Name = name.getText().toString();
+                final String Phone = phone.getText().toString();
 
                 if(Email.isEmpty()  || Pass.isEmpty() || Confirm.isEmpty() || Name.isEmpty() || Phone.isEmpty()){
                     if(Email.isEmpty()){
@@ -72,26 +89,40 @@ public class RegisterOneFragment extends Fragment implements View.OnClickListene
                         if(!mCheckBox.isChecked()){
                             mCheckBox.setError(errorMessage);
                         }
-                        else {
-                            //Part DB Local h3mlha b3den
-//                            boolean result = helper.register(Email, Pass);
-//                            if (!result) {
-//                                Toast.makeText(RegisterActivity.this, "Account Already exists", Toast.LENGTH_LONG).show();
-//                            } else {
-                            email.getText().clear();
-                            password.getText().clear();
-                            confirmPassword.getText().clear();
-                            name.getText().clear();
-                            phone.getText().clear();
-
-                            mCheckBox.setChecked(false);
-                            //      mWebService = new CallingWebService(RegisterActivity.this);
-                            //     String response = mWebService.Register("Register/test");
-                            //   Toast.makeText(RegisterActivity.this, response, Toast.LENGTH_SHORT).show();
-                            ((OnComplete)getActivity()).onComplete(Email);
-                            //  startActivity(new Intent(getContext(), IndexActivity.class));
-
-                            //  }
+                        else{
+                            String url = "https://elmataam.azurewebsites.net/api/Mobile/registerJson";
+                            //String url = "http://10.0.3.2:52183/api/Mobile/registerJson";
+                            StringRequest stringRequest = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+                                @Override
+                                public void onResponse(String response) {
+                                    Toast.makeText(getContext(),response,Toast.LENGTH_SHORT).show();
+                                    if(response.charAt(1) == 's'){
+                                        email.getText().clear();
+                                        password.getText().clear();
+                                        confirmPassword.getText().clear();
+                                        name.getText().clear();
+                                        phone.getText().clear();
+                                        mCheckBox.setChecked(false);
+                                        ((OnComplete)getActivity()).onComplete(Email);
+                                    }
+                                }
+                            }, new Response.ErrorListener() {
+                                @Override
+                                public void onErrorResponse(VolleyError error) {
+                                    Snackbar.make(mCheckBox,error.toString(),Snackbar.LENGTH_LONG).show();
+                                }
+                            }){
+                                @Override
+                                protected Map<String, String> getParams() throws AuthFailureError {
+                                    Map<String,String> params = new HashMap<>();
+                                    params.put("Email",Email);
+                                    params.put("Password",Pass);
+                                    params.put("Name",Name);
+                                    params.put("Phone",Phone);
+                                    return params;
+                                }
+                            };
+                            ApplicationController.getInstance().addToRequestQueue(stringRequest);
                         }
                     }
                     else{
